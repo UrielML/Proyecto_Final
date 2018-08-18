fb8b3961f8d247723a7e7a9f6b92fe350fd94229
Elías Pujol Rodríguez <1715110467@utec-tgo.edu.mx>
2018-08-18 14:58:13 -0500

    Add files via upload

---
+++ proyecto_web/preguntas_frecuentes.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./resources/css/reset.css">
+	<link rel="stylesheet" href="./resources/css/main.css">
+	<title>Preguntas Frecuentes</title>
+</head>
+<body>
+	<div class="baseContainer">
+<?php
+    include('menu_elias.php');
+  ?>
+  	<div class="texto_proyecto">
+       <h3 class="acerca">¿Tienes preguntas acerca del funcionamiento de este sistema?<img  class="imagen_idea" src="./resources/img/preguntas.png"></h3> 
+   <h3 class="acerca_preguntas"> Aquí te enlistamos las preguntas más recurrentes de los usuarios acerca de esta plataforma web</h3>
+   <br>
+      <h3 class="acerca">¿Como funciona?<img  class="imagen_idea" src="./resources/img/preguntas.png"></h3> 
+   <p class="como_funciona">Un administrador  crea un grupo desde la aplicación  o en el panel de control desde la plataforma web, se agregaran a cada uno de los vecinos y enseguida cada integrante descarga la aplicación y se registra. 
+   Despues cuando alguien quiera mandar una alerta  les llegara una notificación a todos los vecinos que estén dentro de la cerca perimetral, sonando una Alarma en su Celular con la información de que usuario activo la alarma. Simultáneamente si cuenta con nuestra Alarma Vecinal se activara para ahuyentar al delincuente y así usted no ponga en riesgo su integridad física. 
+  </p>
+  <br>
+   
+   <h3 class="acerca">¿Cúales son los tipos de alarma con los que cuenta el sistema?<img  class="imagen_idea" src="./resources/img/warning.png"></h3> 
+   <p class="como_funciona">  Las categorías en las que se dividen las tipos de alertas que se mostrarán son: 
+  </p>
+  <br>
+  <br>
+  <ul>
+  <li>Personas sospechosas/no identificadas cerca de la cerca perimetral</li>
+  <li>Mascotas sueltas</li>
+  <li>Tráfico de drogas (venta de drogas)</li>
+  <li>Accidentes domésticos</li>
+  <li>Violencia doméstica/intrafamiliar</li>
+  <li>Extorsiones</li>
+  <li>Abuso sexual/Violaciones</li>
+  <li>Riñas (peleas callejeras)</li>
+  <li>Personas bajo influjo de estupefacientes (Personas Alcoholizadas o Drogadas)</li>
+  <li>Personas perdidas que vivan dentro de la zona</li>
+  <li>Robos a casas</li>
+  <li>Asaltos</li>
+  <li>Robos de automóviles</li>
+  <li>Abusos de autoridad</li>
+</ul>
+<br>
+ 
+  <h3 class="acerca">¿Que pasa cuando mando una alerta errónea o de juego?<img  class="imagen_idea" src="./resources/img/warning.png"></h3> 
+   <p class="como_funciona">Con cualquier uso inadecuado de la aplicación móvil o de la Plataforma Web como por ejemplo, usarla para activar falsas alarmas o alarmas con motivo de juego será motivo de infracción así como también de una sanción, las infracciones serán atendidas de la siguiente manera dependiendo del tipo de usuario: 
+Usuario residente. Las primeras 3 infracciones serán sancionadas por el administrador, a la cuarta infracción se le será negado el servicio (sujeto a aclaraciones). 
+Usuario Administrador. Después de 2 faltas se le negará el servicio (sujeto a aclaraciones).
+
+  </p>
+ 		
+		</div>
+<?php
+		include("footer.php");
+	?>
+    
+</body>
+</html>
